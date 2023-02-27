ea871ec4835d183f2e8af348abcb8a4fa2802d74
NikoMyoji <nicolas.vlte@gmail.com>
2023-02-27 16:43:30 +0100

    integrate XRI in custom scene

---
+++ Assets/Showroom/Scripts/Lever.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public void Activate()
+    {
+        Debug.Log("Hello");
+    }
+    public void Deactivate()
+    {
+        Debug.Log("Bye");
+    }
+}
